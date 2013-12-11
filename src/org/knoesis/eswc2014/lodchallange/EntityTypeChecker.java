3660a798e2bd3cd74cb3b75f51fe471f1c88f650
Sanjaya Wijeratne <sanjrockzz@gmail.com>
2013-12-11 16:14:08 -0500

    Commiting Entity Type Checker - Initial Version

---
+++ src/org/knoesis/eswc2014/lodchallange/EntityTypeChecker.java
@@ -0,0 +1,136 @@
+package org.knoesis.eswc2014.lodchallange;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This class checks what are the types of DBPedia entities that are passed to it and filters out only Books.
+ * 
+ * @author sanjaya@knoesis.org
+ */
+public class EntityTypeChecker
+{
+
+	public static final String BOOKLIST_FILENAME = "data/books.uri";
+
+	private HashMap<String, ArrayList<String>> bookNames;
+
+	public EntityTypeChecker()
+	{
+		this.bookNames = new HashMap<String, ArrayList<String>>();
+		initializeKeywordsMap();
+	}
+
+	/**
+	 * This method initializes the hashmap. All book names are index on their first letter to efficiently access them.
+	 */
+	private void initializeKeywordsMap()
+	{
+		if ( this.bookNames == null )
+		{
+			this.bookNames = new HashMap<String, ArrayList<String>>();
+		}
+		this.bookNames.put( "a", new ArrayList<String>() );
+		this.bookNames.put( "b", new ArrayList<String>() );
+		this.bookNames.put( "c", new ArrayList<String>() );
+		this.bookNames.put( "d", new ArrayList<String>() );
+		this.bookNames.put( "e", new ArrayList<String>() );
+		this.bookNames.put( "f", new ArrayList<String>() );
+		this.bookNames.put( "g", new ArrayList<String>() );
+		this.bookNames.put( "h", new ArrayList<String>() );
+		this.bookNames.put( "i", new ArrayList<String>() );
+		this.bookNames.put( "j", new ArrayList<String>() );
+		this.bookNames.put( "k", new ArrayList<String>() );
+		this.bookNames.put( "l", new ArrayList<String>() );
+		this.bookNames.put( "m", new ArrayList<String>() );
+		this.bookNames.put( "n", new ArrayList<String>() );
+		this.bookNames.put( "o", new ArrayList<String>() );
+		this.bookNames.put( "p", new ArrayList<String>() );
+		this.bookNames.put( "q", new ArrayList<String>() );
+		this.bookNames.put( "r", new ArrayList<String>() );
+		this.bookNames.put( "s", new ArrayList<String>() );
+		this.bookNames.put( "t", new ArrayList<String>() );
+		this.bookNames.put( "u", new ArrayList<String>() );
+		this.bookNames.put( "v", new ArrayList<String>() );
+		this.bookNames.put( "w", new ArrayList<String>() );
+		this.bookNames.put( "x", new ArrayList<String>() );
+		this.bookNames.put( "y", new ArrayList<String>() );
+		this.bookNames.put( "z", new ArrayList<String>() );
+		populateBookNamesHashMap();
+	}
+
+	public void populateBookNamesHashMap()
+	{
+		this.populateBookNamesHashMap( EntityTypeChecker.BOOKLIST_FILENAME );
+	}
+
+	public void populateBookNamesHashMap( String filePath )
+	{
+		BufferedReader reader = null;
+		try
+		{
+			reader = new BufferedReader( new FileReader( filePath ) );
+			String line = null;
+			String dataFields[] = null;
+			String firstString = null;
+
+			ArrayList<String> bookNames = null;
+
+			while ( ( line = reader.readLine() ) != null )
+			{
+				dataFields = line.split( "\t" );
+				if ( dataFields.length > 0 )
+				{
+					firstString = Character.toString( dataFields[0].charAt( 0 ) ).toLowerCase();
+					if ( firstString != null )
+					{
+						bookNames = this.bookNames.get( firstString );
+						// In case a bookname starts with a number or some other character than a letter.
+						if( bookNames == null )
+						{
+							bookNames = new ArrayList<String>();
+						}
+						bookNames.add( dataFields[0] );
+						this.bookNames.put( firstString, bookNames );
+					}
+				}
+			}
+
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				reader.close();
+			}
+			catch ( Exception e )
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public boolean isABookType(String entityName)
+	{
+		String firstString = Character.toString( entityName.charAt( 0 ) ).toLowerCase();
+		ArrayList<String> bookNames = this.bookNames.get( firstString );
+		if( bookNames != null )
+		{
+			return bookNames.contains( entityName );
+		}
+		return false;
+	}
+
+	public static void main(String args[])
+	{
+		EntityTypeChecker typeChecker = new EntityTypeChecker();
+		System.out.println( typeChecker.isABookType( "A_Modest_Proposal" ) );
+	}
+	
+}
