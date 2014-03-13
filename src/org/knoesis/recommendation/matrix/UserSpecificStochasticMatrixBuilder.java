2a998a0616416644df2a045cc0e1b53dddbf5a31
PavanKapanipathi <pavan046@gmail.com>
2014-03-13 12:21:50 -0400

    from 2 mins per iteration to 2 mins per user. User specific matrix creation and it works. Need to check the code for bugs though :D

--- src/org/knoesis/recommendation/matrix/UserSpecificStochasticMatrixBuilder.java
+++ src/org/knoesis/recommendation/matrix/UserSpecificStochasticMatrixBuilder.java
@@ -10,39 +10,40 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-import org.knoesis.userprofile.UserProfileGeneratorFromESWCData;
-import org.knoesis.utils.ProjectVariables;
-import org.mortbay.jetty.servlet.HashSessionIdManager;
-
 public class UserSpecificStochasticMatrixBuilder implements MatrixBuilder{
-	
+
 	private static String userProfileFile, matrixFileName; 
 	private static Set<String> userProfileEntities; 
+	private static Map<String, Double> sumForStochasticEntities = new HashMap<>(); 
+	private static double threshold = 0.05;
 	/**
 	 * Needs the user 
 	 * @param userProfileFile
 	 * @param matrixFileName
 	 */
 	public UserSpecificStochasticMatrixBuilder(String userProfileFile, String matrixFileName) {
-		this.userProfileFile = ProjectVariables.strdataFolder+File.separator
-				+ProjectVariables.strUserFolder+File.separator+2;; 
+		this.matrixFileName = matrixFileName; 
+		this.userProfileFile = userProfileFile; 
+		//for now 
+		this.matrixFileName = "data/dbpedia/nier-3-dec.tsv"; 
+		this.userProfileFile = "data/dbpedia/706";
 		readUserProfile(); 
 		//For now 
-    	this.matrixFileName = ProjectVariables.strdataFolder+File.separator
-		+ProjectVariables.strInputSimilarityFolder+File.separator+ProjectVariables.inputSimilarityFile;
+		//this.matrixFileName = ProjectVariables.strdataFolder+File.separator
+		//+ProjectVariables.strInputSimilarityFolder+File.separator+ProjectVariables.inputSimilarityFile;
 	}
 	/**
 	 * 
 	 */
 	private void readUserProfile() {
-		userProfileEntities = new HashSet<String>();
+		userProfileEntities = new HashSet<>();
 		BufferedReader read;
 		String line=null; 
 		try {
-			read = new BufferedReader(new FileReader(new File(this.userProfileFile)));
+			read = new BufferedReader(new FileReader(this.userProfileFile));
 			while((line = read.readLine())!=null){
-				String[] objects = line.split("\t"); 
-				userProfileEntities.add(objects[0]); 
+				String[] objects = line.split("\t");
+				userProfileEntities.add("<"+objects[0].trim()+">"); 
 			}
 			read.close();
 		} catch (FileNotFoundException e) {
@@ -57,53 +58,43 @@ public class UserSpecificStochasticMatrixBuilder implements MatrixBuilder{
 	@Override
 	public Map<String, HashMap<String, Double>> mapMatrixBuilder() {
 		String strFileName = this.matrixFileName;
-        BufferedReader read;
-    	Map<String, HashMap<String, Double>> matrix = new HashMap<String, HashMap<String,Double>>();
-	     
+		Map<String, HashMap<String, Double>> matrix = new HashMap<String, HashMap<String,Double>>();
+		Set<String> allEntities = getAllIUserEntities(); 
+		for(String entity: allEntities){
+			HashMap<String, Double> relatedBooks = new HashMap<>();
+			sumForStochasticEntities.put(entity, 0.0d);
+			matrix.put(entity, relatedBooks); 
+		}
+		BufferedReader read;
+
 		try {
 			read = new BufferedReader(new FileReader(strFileName));
-		   String line = null;
-	        int i=0;
-	        
-	        while((line = read.readLine())!=null){
-	            i++;
-	            String[] objects = line.split("\t");
-	            HashMap<String, Double> relatedEntitiesScore;
-	            //HashMap<String, Double> relatedEntitiesScore2 = new HashMap<>();
-	            // For A
-	            if(matrix.keySet().contains(objects[0].trim())){
-	                relatedEntitiesScore = matrix.get(objects[0].trim());
-	                relatedEntitiesScore.put(objects[1].trim(), Double.parseDouble(objects[2].trim()));
-	                Double sum = relatedEntitiesScore.get("sum") + Double.parseDouble(objects[2].trim());
-	                relatedEntitiesScore.put("sum", sum);    
-	                matrix.put(objects[0].trim(), relatedEntitiesScore); 
-	            } else{
-	                relatedEntitiesScore = new HashMap<>();
-	                relatedEntitiesScore.put(objects[1].trim(), Double.parseDouble(objects[2].trim()));
-	                relatedEntitiesScore.put("sum", Double.parseDouble(objects[2].trim()));
-	                matrix.put(objects[0].trim(), relatedEntitiesScore); 
-	            }
-	            // For B
-	            //relatedEntitiesScore = new HashMap<>();
-	            if(matrix.keySet().contains(objects[1].trim())){
-	                relatedEntitiesScore = matrix.get(objects[1].trim());
-	                relatedEntitiesScore.put(objects[0].trim(), Double.parseDouble(objects[2].trim()));
-	                Double sum = relatedEntitiesScore.get("sum") + Double.parseDouble(objects[2].trim());
-	                relatedEntitiesScore.put("sum", sum);    
-	                matrix.put(objects[1].trim(), relatedEntitiesScore); 
-	            } else{
-	                relatedEntitiesScore = new HashMap<>();
-	                relatedEntitiesScore.put(objects[0].trim(), Double.parseDouble(objects[2].trim()));
-	                relatedEntitiesScore.put("sum", Double.parseDouble(objects[2].trim()));
-	                matrix.put(objects[1].trim(), relatedEntitiesScore); 
-	            }
-	            
-	            if(i%100000 == 0)
-	                System.out.println(i+":"+System.currentTimeMillis());
-	        }
-	        read.close();
-	        System.out.println("Total Number of Books: "+matrix.keySet().size());
-	        
+			String line = null;
+			int i=0;
+
+			while((line = read.readLine())!=null){
+				i++;
+				String[] objects = line.split("\t");
+				double score = Double.parseDouble(objects[2]);
+				if(score>threshold){
+					if(matrix.keySet().contains(objects[0])){
+						matrix.get(objects[0]).put(objects[1], score);
+						double sum = sumForStochasticEntities.get(objects[0]) + score;
+						sumForStochasticEntities.put(objects[0], sum);
+					}
+					if(matrix.keySet().contains(objects[1])){
+						matrix.get(objects[1]).put(objects[0], score);
+						double sum = sumForStochasticEntities.get(objects[1]) + score;
+						sumForStochasticEntities.put(objects[1], sum);
+					}
+				}
+				if(i%1000000 == 0)
+					System.out.println(i+": "+matrix.keySet().size()+": "+ System.currentTimeMillis());
+			}
+			addDanglingEntitiesToMatrix(matrix); 
+			makeMatrixStochastic(matrix);
+			read.close();
+			System.out.println("Total Number of Books: "+matrix.keySet().size());
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (NumberFormatException e) {
@@ -111,24 +102,93 @@ public class UserSpecificStochasticMatrixBuilder implements MatrixBuilder{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		removeUserNonSpecificNodes(matrix); 
-        return matrix;
-   }
-	/**
-	 * Remove entities that are not at hop 2 from the profiled entities
-	 * @param matrix
-	 */
-	private void removeUserNonSpecificNodes(
-			Map<String, HashMap<String, Double>> matrix) {
-		Set<String> userEntities = new HashSet<>(); 
-		userEntities.addAll(userProfileEntities); 
-		for(String entity: userProfileEntities){
-			userEntities.addAll(matrix.get(entity).keySet());
-		}
-		for(String entity:matrix.keySet()){
-			if(!userEntities.contains(entity))
-				matrix.remove(entity);
-		}
+		return matrix;
 	}
 
+	// 
+	/**
+	 * 
+	 * @param matrix
+	 */
+	private void makeMatrixStochastic(
+			Map<String, HashMap<String, Double>> matrix) {
+		Set<String> books = new HashSet<String>(matrix.keySet());
+		for(String book: books){
+			double sum = sumForStochasticEntities.get(book);
+			//removing books of user profile that do not have any related books 
+			if(matrix.get(book).isEmpty()){
+				matrix.remove(book);
+				continue;
+			}
+
+			Set<String> relatedBooks = new HashSet<>(matrix.get(book).keySet()); 
+			for(String relatedBook: relatedBooks){
+				matrix.get(book).put(relatedBook, matrix.get(book).get(relatedBook)/sum);
+			}
+		}
+	}
+	/**
+	 * completing the graph by adding edges to dangling links.
+	 * @param matrix
+	 */
+	private void addDanglingEntitiesToMatrix(
+			Map<String, HashMap<String, Double>> matrix) {
+		Set<String> danglingLinks = new HashSet<>(); 
+		Set<String> entities = new HashSet<>(matrix.keySet()); 
+		for(String entity: entities){
+			for(String danglingLink: matrix.get(entity).keySet()){
+				if(!matrix.keySet().contains(danglingLink)){
+					danglingLinks.add(danglingLink); 
+					HashMap<String, Double> relatedLinks = new HashMap<String, Double>();
+					relatedLinks.put(entity, matrix.get(entity).get(danglingLink)); 
+					sumForStochasticEntities.put(danglingLink, matrix.get(entity).get(danglingLink));
+					matrix.put(danglingLink, relatedLinks);
+				} else if(danglingLinks.contains(danglingLink)){
+					double sum = sumForStochasticEntities.get(danglingLink) + matrix.get(entity).get(danglingLink);
+					matrix.get(danglingLink).put(entity, matrix.get(entity).get(danglingLink)); 
+					sumForStochasticEntities.put(danglingLink, sum);
+				}
+			}
+		}
+	}
+	/**
+	 * Gets all the entities that have to be considered
+	 * @return
+	 */
+	private Set<String> getAllIUserEntities() {
+		Set<String> allEntities = new HashSet<>(); 
+		try {
+			BufferedReader read = new BufferedReader(new FileReader(new File(this.matrixFileName)));
+			String line = null; 
+			int i=0;
+			while((line=read.readLine())!=null){
+				i++;
+				String objects[] = line.split("\t");
+				double score = Double.parseDouble(objects[2]);
+				if(score>threshold){
+					if(userProfileEntities.contains(objects[0]))
+						allEntities.add(objects[1]); 
+					else if (userProfileEntities.contains(objects[1]))
+						allEntities.add(objects[0]);
+				}
+				if(i%1000000==0)
+					System.out.println("Entities got for this user at " + i +" "+allEntities.size());
+			}
+			System.out.println("Entities got for this user at " + i +" "+allEntities.size());
+			read.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		allEntities.addAll(userProfileEntities);
+		return allEntities;
+	}
+
+	public static void main(String[] args) {
+		MatrixBuilder matrix = new UserSpecificStochasticMatrixBuilder(args[0], args[1]); 
+		matrix.mapMatrixBuilder(); 
+	}
 }
--- src/org/knoesis/recommendations/pagerank/EntityRankCalculator.java
+++ src/org/knoesis/recommendations/pagerank/EntityRankCalculator.java
@@ -27,58 +27,96 @@ public class EntityRankCalculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		EntityRankCalculator aRankCal = new EntityRankCalculator();
-		aRankCal.entityRankingUserMap();
-		
+		aRankCal.entityRankingUserMapEachUser();
+
 	}
-	
+
 	/**
 	 * Main conrtroller method will iterate over each users rating profile and create a ranked list of 
 	 * books for each user
 	 */
-	 
+
 	public void entityRankingUserMap(){
 		// FIXME: added a null object for StochasticMatrixBuilder
 		//StochasticMatrixBuilder aStochasticMatrixBuilder = new StochasticMatrixBuilder(null);
-		UserSpecificStochasticMatrixBuilder aStochasticMatrixBuilder = new UserSpecificStochasticMatrixBuilder(null, null);
+		UserSpecificStochasticMatrixBuilder aStochasticMatrixBuilder = 
+				new UserSpecificStochasticMatrixBuilder(null, null);
 		Map<String, HashMap<String, Double>> matrix = aStochasticMatrixBuilder.mapMatrixBuilder();
-		
+
 		ResultHandler aResultHandler = new ResultHandler();
-		
-		
+
+
 		//User profiles
 		//Iterate over every user profile and calculate the rank
 		File folder = new File(ProjectVariables.strdataFolder+File.separator
-						+ProjectVariables.strUserFolder);
+				+ProjectVariables.strUserFolder);
 		File[] listOfFiles = folder.listFiles(); 
-		
+
 		for (int i = 0; i < listOfFiles.length; i++) {//each file contains a user profile
-			 if (listOfFiles[i].isFile()) {
+			if (listOfFiles[i].isFile()) {
 				String strFileName = listOfFiles[i].getName();
-				
+
 				//Assign an initial rank to each book 
 				Map<String, Double> mapEntityRank = new HashMap<String, Double>();
 				for(String aElement: matrix.keySet()){
 					mapEntityRank.put(aElement, 1.00);
 				}
-					
-				 //Get the relevant User profile
-				 UserRatingCreator aUserRator = new UserRatingCreator();
-				 Map<String, Double> mapOfUserProfile = aUserRator.userProfileGenerator(listOfFiles[i]);
-				 
-				 //Calculate the personalized page rank
-				 System.out.println("Begin page rank"+System.currentTimeMillis());
-				 Map<String, Double> newPersonalizedRank = calculateEntityRankMap(matrix, mapEntityRank, mapOfUserProfile);
-				 System.out.println("End page rank"+System.currentTimeMillis());
-				 
-				 //Print the results
-				 aResultHandler.resultRanking(newPersonalizedRank, strFileName);
-				 
-			 }
+
+				//Get the relevant User profile
+				UserRatingCreator aUserRator = new UserRatingCreator();
+				Map<String, Double> mapOfUserProfile = aUserRator.userProfileGenerator(listOfFiles[i]);
+
+				//Calculate the personalized page rank
+				System.out.println("Begin page rank"+System.currentTimeMillis());
+				Map<String, Double> newPersonalizedRank = calculateEntityRankMap(matrix, mapEntityRank, mapOfUserProfile);
+				System.out.println("End page rank"+System.currentTimeMillis());
+
+				//Print the results
+				aResultHandler.resultRanking(newPersonalizedRank, strFileName);
+
+			}
 		}
-		
-		
+
+
 	}
-	
+	/**
+	 * Replicated the above function for testing 
+	 * PAVAN :D
+	 */
+	public void entityRankingUserMapEachUser(){
+		// FIXME: added a null object for StochasticMatrixBuilder
+		//StochasticMatrixBuilder aStochasticMatrixBuilder = new StochasticMatrixBuilder(null);
+		UserSpecificStochasticMatrixBuilder aStochasticMatrixBuilder = 
+				new UserSpecificStochasticMatrixBuilder(null, null);
+		Map<String, HashMap<String, Double>> matrix = aStochasticMatrixBuilder.mapMatrixBuilder();
+
+		ResultHandler aResultHandler = new ResultHandler();
+
+
+
+		//Assign an initial rank to each book 
+		Map<String, Double> mapEntityRank = new HashMap<String, Double>();
+		for (String book: matrix.keySet()){
+			mapEntityRank.put(book, 1.00); 
+			for(String aElement: matrix.get(book).keySet()){
+				mapEntityRank.put(aElement, 1.00);
+			}
+		}
+
+		//Get the relevant User profile
+		UserRatingCreator aUserRator = new UserRatingCreator();
+		Map<String, Double> mapOfUserProfile = aUserRator.userProfileGenerator(new File("data/dbpedia/2"));
+
+		//Calculate the personalized page rank
+		System.out.println("Begin page rank"+System.currentTimeMillis());
+		Map<String, Double> newPersonalizedRank = calculateEntityRankMap(matrix, mapEntityRank, mapOfUserProfile);
+		System.out.println("End page rank"+System.currentTimeMillis());
+
+		//Print the results
+		aResultHandler.resultRanking(newPersonalizedRank, "data/dbpedia/706.ranking");
+
+	}
+
 	/**
 	 * * This method will calculate the ranking of each
 	 * entity after a given number of iteration based on a give user's profile
@@ -89,35 +127,34 @@ public class EntityRankCalculator {
 	 */
 	public Map<String, Double> calculateEntityRankMap(Map<String, HashMap<String, Double>> mapOfSimilarity,
 			Map<String, Double> mapEntityRank, Map<String, Double> mapUserRating){
-		
+
 		int iIteCount = 0;
-		
+
 		while(iIteCount < ProjectVariables.MAX_ITERATIONS){ //LOOP will iterate over MAX_ITERATIONS
-			
+
 			Set<String> aSetofEntities = mapEntityRank.keySet();
 			for(String aEntity: aSetofEntities){
-				
+
 				double dWeightedSum = 0.0;
 				double dUserRating = 0.0;
 				double dCurrentRankValue = mapEntityRank.get(aEntity);
-				
+
 				if(mapUserRating.containsKey(aEntity)){
 					dUserRating = mapUserRating.get(aEntity);
 				}
-				
+
 				//Get the connected edges and calculate the weighted sum 
 				Map<String, Double> connectedEdges = mapOfSimilarity.get(aEntity);
+
 				Set<String> connectedEntities = connectedEdges.keySet();
-				
 				for(String aConnection : connectedEntities){
-					if(!aConnection.equalsIgnoreCase("sum")){
-						Map<String, Double> aMapOfConnectedEdges = mapOfSimilarity.get(aConnection);
-						double dValue = aMapOfConnectedEdges.get(aEntity);
-						double dSum = aMapOfConnectedEdges.get("sum");
-						dWeightedSum = dWeightedSum + dCurrentRankValue*(dValue/dSum);
-					}
+					Map<String, Double> aMapOfConnectedEdges = mapOfSimilarity.get(aConnection);
+					double dValue = aMapOfConnectedEdges.get(aEntity);
+					//This should be taken care of while building the matrix
+					//double dSum = aMapOfConnectedEdges.get("sum");
+					dWeightedSum = dWeightedSum + dCurrentRankValue*(dValue);
 				}
-				
+
 				//Calculate the new rank value
 				double dEntityNewRank = (1 - ProjectVariables.DAMPING_FACTOR) * dUserRating + //personalized page rank
 						ProjectVariables.DAMPING_FACTOR * dWeightedSum;
@@ -125,19 +162,19 @@ public class EntityRankCalculator {
 				if(mapEntityRank.containsKey(aEntity))
 					if(dEntityNewRank == mapEntityRank.get(aEntity))
 						continue;
-				
+
 				mapEntityRank.put(aEntity, dEntityNewRank);
-				
+
 			}
 			System.out.println("Iteration :"+iIteCount);
 			iIteCount++;
-			
+
 		}
-		
+
 		return mapEntityRank;
 	}
-	
-	
+
+
 	/**
 	 * This method will iterate over each users rating profile and create a ranked list of 
 	 * book ratings and print the output which is a ranked list of books for each user
@@ -147,7 +184,7 @@ public class EntityRankCalculator {
 	 */
 
 	/*public void entityRankingPerUser(){
-		
+
 		//Load the row stochastic matrix
 		double[][] rowStochasticSimilarityMatrix = (double[][]) Serializer.load(ProjectVariables.strdataFolder+File.separator+
 				ProjectVariables.strSerialzedDataFolder+File.separator+
@@ -159,12 +196,12 @@ public class EntityRankCalculator {
 		for(int i = 0; i < iLength; i++){
 			initRankVector[i] = 1.00;
 		}
-		
+
 		//Iterate over every user profile and calculate the rank
 		File folder = new File(ProjectVariables.strdataFolder+File.separator
 				+ProjectVariables.strUserFolder);
 		File[] listOfFiles = folder.listFiles(); 
-		
+
 		for (int i = 0; i < listOfFiles.length; i++) {
 			 if (listOfFiles[i].isFile()) {
 				 String strFileName = listOfFiles[i].getName();
@@ -173,12 +210,12 @@ public class EntityRankCalculator {
 				 double[] personalizedBookRank = calculateEntityRank(rowStochasticSimilarityMatrix,
 						 initRankVector, userRatingVector);
 				// aResultHandler.resultRanking(personalizedBookRank, strFileName);
-				 
-				 
+
+
 			 }
 		 }		
 	}*/
-	
+
 	/**
 	 * . 
 	 * This method will calculate the ranking of each
@@ -189,42 +226,42 @@ public class EntityRankCalculator {
 	 * @param Normalized user ratings
 	 * @return Ranked Entities
 	 */
-	
+
 	/*public double[] calculateEntityRank(double[][] rowStochasticSimilarityMatrix, 
 			double[] entityRankVector, double[] userRating){
-		
+
 		int iIteCount = 0;
 		int iEntityCount = entityRankVector.length;
-	
+
 		while(iIteCount < ProjectVariables.MAX_ITERATIONS){ //LOOP will iterate over MAX_ITERATIONS
-			
+
 			for(int iElement = 0; iElement < iEntityCount; iElement++){
-				
+
 				double dWeightedSum = 0.0;
 				double dUserRating = userRating[iElement];
-				
+
 				for (int j = 0; j < iEntityCount; j++){ // Get all the connected edges for the given entity 
-					
+
 					if(rowStochasticSimilarityMatrix[iElement][j]!= 0){ // calculate the sum of contributing value from each connected edge
 						dWeightedSum = dWeightedSum + entityRankVector[j] * 
 								rowStochasticSimilarityMatrix[j][iElement];
-						
+
 					}
 				}
-				
+
 				double dEntityNewRank = (1 - ProjectVariables.DAMPING_FACTOR) * dUserRating + //personalized page rank
 						ProjectVariables.DAMPING_FACTOR * dWeightedSum;	
 				entityRankVector[iElement] = dEntityNewRank;
-				
+
 			}
 			iIteCount++;
 		}
-		
+
 		for(double dRank : entityRankVector){
 			System.out.println(dRank);
 		}
 		return entityRankVector;
-		
+
 	}*/
-	
+
 }
